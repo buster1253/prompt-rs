8903bb4dff9e54f7a7344dd534ae11091995f36d
buster1253 <petterdberven@hotmail.com>
2019-09-29 14:40:05 +0200

    redid the jobs command

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,18 +1,26 @@
-use std::io::{BufReader, BufRead};
+use std::io::BufRead;
 use std::os::unix::process::parent_id;
 use std::process::{Command, Stdio};
 
 pub fn get_jobs() -> Option<String> {
-    let ppid = parent_id();
-    let mut cmd = Command::new("ps")
-        .arg("--ppid")
-        .arg(ppid.to_string())
-        .arg("-oppid=")
+    let ppid = parent_id().to_string();
+    let cmd = match Command::new("ps")
+        .args(&["--ppid", ppid.as_str(), "-oppid="])
         .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+        .output()
+        {
+            Ok(output) => output,
+            Err(err) => {
+                println!("Error getting jobs: {}", err);
+                return None
+            }
+    };
 
-    let stdout = cmd.stdout.as_mut().unwrap();
-    let stdout_reader = BufReader::new(stdout);
-    Some((stdout_reader.lines().count() - 1).to_string())
+    let lines = cmd.stdout.lines().count() - 1;
+    if lines > 0 {
+        Some(lines.to_string())
+    }
+    else {
+        None
+    }
 }
